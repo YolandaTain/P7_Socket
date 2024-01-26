8ed634763e246776c1a6156abbdaef0a30496149
YolandaTain <yolandataingut@gmail.com>
2024-01-26 08:53:14 +0100

    aub

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-const express = require("express");
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const {Server} = require('socket.io');
-const io = new Server(server);
-io.on('connection', (socket) => {
-console.log('¡Un usuario se ha conectado')
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {}
 });
-/*
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get('/', (req, res) => {
-//res.send("")
-res.sendFile(`${__dirname}/cliente/ind
-ex.html`)
+  res.sendFile(join(__dirname, 'index.html'));
 });
-*/
-server.listen(3000, ()=> {
-console.log('Servidor corriendo en http://localhost:3000')
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
 });
